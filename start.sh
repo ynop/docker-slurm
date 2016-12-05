2e4f230bf2278935aeecdbf52c7be6cdf69f7b29
buec <buec@zhaw.ch>
2016-12-05 15:50:21 +0100

    some adjustments

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
 #docker run -d --net="host" --name slurm -v /grid:/home srv-fs-t-610.zhaw.ch:5555/slurmctl
-docker run -d --net="host" --name slurm -v /grid:/home ynop/slurmctl
+
+# Linux
+docker run -d -p 6817:6817 -p 6818:6818 -p 57000-59999:57000-59999 --name slurm -v /grid:/home ynop/slurmctl
+
+# OSX
+docker run -d -p 6817:6817 -p 6818:6818 -p 57000-59999:57000-59999 --privileged=true --name slurm ynop/slurmd
